dad1693d6ae1abc6eda285418eab111a2b9b9c7f
svilku <sonal.vilku20@gmail.com>
2023-12-05 15:57:26 +0530

    ack consumed msgs

--- src/main/java/org/design/listener/msglisterner/service/StaleTextMessageConsumer.java
+++ src/main/java/org/design/listener/msglisterner/service/StaleTextMessageConsumer.java
@@ -11,5 +11,6 @@ public class StaleTextMessageConsumer extends TextMessageConsumer {
     @Override
     protected void processMessage(Message message) throws JMSException {
         log.info("Stale Message : [{}] received with id: {}", message.getBody(String.class), message.getJMSMessageID());
+        message.acknowledge();
     }
 }
